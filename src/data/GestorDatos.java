703c4e783b41c18efd00852f58b122e0141cfd43
tofer <tofervicente@gmail.com>
2026-06-29 14:33:54 -0400

    Arreglo

--- src/data/GestorDatos.java
+++ src/data/GestorDatos.java
@@ -9,48 +9,10 @@ import java.util.ArrayList;
 
 public class GestorDatos {
 
-    public ArrayList<Tour> cargarTours() {
-
-        ArrayList<Tour> listaTours = new ArrayList<>();
-
-        try {
-
-            BufferedReader br =
-                    new BufferedReader(
-                            new FileReader("src/resources/tours.txt"));
-
-            String linea;
-
-            while ((linea = br.readLine()) != null) {
-
-                String[] datos = linea.split(";");
-
-                String nombre = datos[0];
-                String destino = datos[1];
-                int precio = Integer.parseInt(datos[2]);
 
 
-                Guia guia = new Guia(
-                        datos[3],
-                        datos[4]
-                );
 
-                Tour tour = new Tour(
-                        nombre,
-                        destino,
-                        precio,
-                        guia
-                );
 
-                listaTours.add(tour);
-            }
 
-            br.close();
 
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-
-        return listaTours;
-    }
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -9,34 +9,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        GestorDatos gestor = new GestorDatos();
 
-        ArrayList<Tour> tours = gestor.cargarTours();
 
-        System.out.println("=== TODOS LOS TOURS ===");
 
-        for (Tour t : tours) {
-            System.out.println(t);
-        }
-
-        System.out.println("\n=== TOURS MAYORES A 18000 ===");
-
-        for (Tour t : tours) {
-
-            if (t.getPrecio() > 18000) {
-                System.out.println(t);
-            }
-
-        }
-
-        System.out.println("\n=== TOURS EN PUERTO VARAS ===");
-
-        for (Tour t : tours) {
-
-            if (t.getDestino().equalsIgnoreCase("Puerto Varas")) {
-                System.out.println(t);
-            }
-
-        }
-    }
+        
 }
